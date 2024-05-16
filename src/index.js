15ef12802914b6955eebad19f1159224bf0d0291
Yuvraj Singh <iamyuvrajsingh118@gmail.com>
2024-05-16 17:45:38 +0530

    props

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const h1 = react.createElement("h1", {style:{color:"red",fontSize:"100px"},id:"heading"}, "hello world");
-
-const div=react.createElement("div",{},h1)
+const p=react.createElement("p",{style:{color:"blue"}},"paragraph");
+const div=react.createElement("div",{},[h1,p]);
 
 
 root.render(div);
--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,39 @@ import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const h1 = <h1 className="heading" style={{fontSize:"100px"}}>Heading</h1>
+const h1 = <h1 className="heading" style={{ fontSize: "100px" }}>Heading</h1>
 
-root.render(h1);
+const H1 = ({data}) => {
+    return (
+        <div>
+            <h1>{data}</h1>
+        </div>
+    )
+}
+
+// */ const Body = () => {
+//     return (
+//         <div>
+//?             <H1 />
+//?             <H1></H1>
+//?             {H1()}
+//?             {h1}
+//         </div>
+//     )
+// }
+const Body = () => {
+    return (
+        <div>
+            <H1 data="yuvraj"/>
+            <H1 data="Age"/>
+            <H1 data="class"/>
+            <H1 data="row"/>
+            <H1 data="col"/>
+            <H1 data="dasd"/>
+            <H1 data="dfd"/>
+            <H1 data="sdsds"/>
+        </div>
+    )
+}
+
+root.render(<Body />);
